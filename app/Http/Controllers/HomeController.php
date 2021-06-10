72f30789d1603aec332c0937ef44aa456c429bf9
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-10 14:23:49 +0200

    style

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index(){
         $movies = Movie::all();
-        dump($movies);
+        //dump($movies);
         return view ('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>films</h1>
+        <h1>Films</h1>
         <div class="container">
+            @foreach ($movies as $movie)
             <div class="card">
-                @foreach ($movies as $movie)
-                    <p>{{ $movie['title']}}</p>
-                @endforeach
+                <h2>{{ $movie['title']}}</h2>
+                <p>{{ $movie['original_title']}}</p>
+                <p>{{ $movie['nationality']}}</p>
+                <p>{{ $movie['date']}}</p>
+                <p>{{ $movie['vote']}}</p>
             </div>
+            @endforeach
         </div>
     </body>
 </html>
